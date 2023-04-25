673d5fbaa8cd2278ce695f31fff7d6091bc6c43f
kassioba <kassioluna@gmail.com>
2023-04-25 10:27:14 -0300

    Atualizações para deploy

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 import express from 'express'
 import cors from "cors";
 import router from "./routes/index.routes.js";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 
@@ -8,4 +11,4 @@ app.use(cors());
 app.use(express.json());
 app.use(router);
 
-app.listen(5000);
+app.listen(process.env.PORT);
